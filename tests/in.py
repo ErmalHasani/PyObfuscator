aba31b268c655964398568d3ab225cb960d80d8c
Ermal Hasani <114736696+ErmalHasani@users.noreply.github.com>
2024-08-17 21:26:31 +0200

    Update in.py

--- tests/in.py
+++ tests/in.py
@@ -1,18 +1,7 @@
-# example.py
+x = input("First number: ")
+y = input("Second number: ")
 
-def greet(name):
-    return f"Hello, {name}!"
-
-def add_numbers(a, b):
-    result = a + b
-    return result
-
-def main():
-    name = "Alice"
-    number1 = 10
-    number2 = 20
-    print(greet(name))
-    print(f"The sum of {number1} and {number2} is {add_numbers(number1, number2)}")
-
-if __name__ == "__main__":
-    main()
+if x > y :
+  print("true")
+else:
+   print("false")
