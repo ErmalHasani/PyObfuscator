a68eff90eb1cdbbe1507f90a8bbd670297f1e4f3
Ermal Hasani <114736696+ErmalHasani@users.noreply.github.com>
2024-08-17 21:26:22 +0200

    Update out.py

--- tests/out.py
+++ tests/out.py
@@ -1,15 +1,6 @@
-def __obf_0001__(name):
-    return f'Hello, {name}!'
-
-def __obf_0002__(a, b):
-    result = a + b
-    return result
-
-def __obf_0003__():
-    name = __builtins__.__import__("base64").b64decode(b"QWxpY2U=").decode()
-    number1 = 10
-    number2 = 20
-    print(__obf_0001__(name))
-    print(f'The sum of {number1} and {number2} is {__obf_0002__(number1, number2)}')
-if __name__ == __builtins__.__import__("base64").b64decode(b"X19tYWluX18=").decode():
-    __obf_0003__()
+x = input(__builtins__.__import__("base64").b64decode(b"Rmlyc3QgbnVtYmVyOiA=").decode())
+y = input(__builtins__.__import__("base64").b64decode(b"U2Vjb25kIG51bWJlcjog").decode())
+if x > y:
+    print(__builtins__.__import__("base64").b64decode(b"dHJ1ZQ==").decode())
+else:
+    print(__builtins__.__import__("base64").b64decode(b"ZmFsc2U=").decode())
